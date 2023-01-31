538fb59c5e960e64e03a0944cbe403ad6e422c1f
taeheeL <t9h9@naver.com>
2023-01-31 16:06:59 +0900

    save image DIR

--- app/src/main/java/com/example/memorialplace/AddMemorialPlaceActivity.kt
+++ app/src/main/java/com/example/memorialplace/AddMemorialPlaceActivity.kt
@@ -5,6 +5,8 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
@@ -23,7 +25,10 @@ import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -220,6 +225,12 @@ class AddMemorialPlaceActivity :
                         val selectedImageBitmap =
                             MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
 
+                        // 이미지를 저장
+                        val saveImageToInternalStorage =
+                            saveImageToInternalStorage(selectedImageBitmap)
+
+                        Log.e("Saved image: ", "Path :: $saveImageToInternalStorage")
+
                         binding.ivPlaceImage!!.setImageBitmap(selectedImageBitmap) // Set the selected image from GALLERY to imageView.
                     } catch (e: IOException) {
                         e.printStackTrace()
@@ -231,6 +242,12 @@ class AddMemorialPlaceActivity :
             } else if (requestCode == CAMERA) {
 
                 val thumbnail: Bitmap = data!!.extras!!.get("data") as Bitmap // Bitmap from camera
+
+                // 이미지를 저장
+                val saveImageToInternalStorage =
+                    saveImageToInternalStorage(thumbnail)
+
+                Log.e("Saved image: ", "Path :: $saveImageToInternalStorage")
                 binding.ivPlaceImage!!.setImageBitmap(thumbnail) // Set to the imageView.
             }
         } else if (resultCode == Activity.RESULT_CANCELED) {
@@ -245,10 +262,30 @@ class AddMemorialPlaceActivity :
         binding.etDate.setText(sdf.format(cal.time).toString())
     }
 
+    private fun saveImageToInternalStorage(bitmap: Bitmap): Uri {
+        // context 확장
+        val wrapper = ContextWrapper(applicationContext)
+        var file = wrapper.getDir(IMAGE_DIRECTORY, Context.MODE_PRIVATE)
+        // 무작위 ID로 이미지 저장
+        file = File(file, "${UUID.randomUUID()}.jpg")
+
+        try {
+            // 압축
+            val stream: OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return Uri.parse(file.absolutePath)
+    }
+
 
     companion object {
         private const val GALLERY = 1
         private const val CAMERA = 2
+        private const val IMAGE_DIRECTORY = "MemorialPlaceImages"
     }
 
 }
