4adf95b9b8fc77d03adcb0a00f56964a4e95f4cf
taeheeL <t9h9@naver.com>
2023-01-31 15:52:41 +0900

    take photo with Dexter (Deprecated)

--- app/src/main/java/com/example/memorialplace/AddMemorialPlaceActivity.kt
+++ app/src/main/java/com/example/memorialplace/AddMemorialPlaceActivity.kt
@@ -1,35 +1,254 @@
 package com.example.memorialplace
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.OnBackPressedCallback
+import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import coil.load
 import com.example.memorialplace.databinding.ActivityAddMemorialPlaceBinding
 import com.example.memorialplace.utill.BindingActivity
-import timber.log.Timber
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class AddMemorialPlaceActivity :
-    BindingActivity<ActivityAddMemorialPlaceBinding>(R.layout.activity_add_memorial_place) {
-    private val TAG = this.javaClass.simpleName
-    //콜백 인스턴스 생성
-    private val callback = object : OnBackPressedCallback(false) {
-        override fun handleOnBackPressed() {
-            // 뒤로 버튼 이벤트 처리
-            finish()
-            Timber.e(TAG, "뒤로가기 클릭")
+    BindingActivity<ActivityAddMemorialPlaceBinding>(R.layout.activity_add_memorial_place),
+    View.OnClickListener {
+
+//    private val TAG = this.javaClass.simpleName
+//    //콜백 인스턴스 생성
+//    private val callback = object : OnBackPressedCallback(false) {
+//        override fun handleOnBackPressed() {
+//            // 뒤로 버튼 이벤트 처리
+//            finish()
+//            Timber.e(TAG, "뒤로가기 클릭")
+//        }
+//    }
+
+
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
+    private val storagePermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                galleryLauncher.launch("image/*")
+            }
         }
+
+    private val galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        binding.ivPlaceImage.load(it)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setSupportActionBar(binding.toolbarAddPlace)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         binding.toolbarAddPlace.setNavigationOnClickListener {
-            onBackPressed()
             // TODO deprecated 된 거 고치기
-            this.onBackPressedDispatcher.addCallback(this, callback)
+            onBackPressed()
+//            this.onBackPressedDispatcher.addCallback(this, callback)
+        }
+
+        dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
+        binding.etDate.setOnClickListener(this)
+        binding.tvAddImage.setOnClickListener(this)
+
+
+//        getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+//            if (it.resultCode == RESULT_OK) {
+//                //BitmapFactory.decodeFile( path ) : image파일을 decoding한다.
+////                BitmapFactory.decodeFile(photoFile.absolutePath)?.let {
+////                    binding.ivPlaceImage.setImageBitmap(it)
+////                }
+//                binding.ivPlaceImage.load(photoFile)
+//            } else {
+//                Toast.makeText(this, "취소 되었습니다", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+    }
+
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.et_date -> {
+                DatePickerDialog(
+                    this@AddMemorialPlaceActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+
+            R.id.tv_add_image -> {
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItems =
+                    arrayOf("Select photo from Gallery", "Capture photo from camera")
+                pictureDialog.setItems(pictureDialogItems) { _, which ->
+                    when (which) {
+                        0 -> choosePhotoFromGallery()
+                        1 -> takePhotoFromCamera()
+                    }
+                }
+                pictureDialog.show()
+            }
         }
 
     }
 
+    private fun choosePhotoFromGallery() {
+//        storagePermission.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        Dexter.withContext(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+
+                    // Here after all the permission are granted launch the gallery to select and image.
+                    if (report!!.areAllPermissionsGranted()) {
+
+                        // TODO(Step 1 : Adding an image selection code here from Gallery or phone storage.)
+                        // START
+                        val galleryIntent = Intent(
+                            Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                        )
+
+                        startActivityForResult(galleryIntent, GALLERY)
+                        // END
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread()
+            .check()
+    }
+
+    private fun takePhotoFromCamera() {
+        Dexter.withContext(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    // Here after all the permission are granted launch the CAMERA to capture an image.
+                    if (report!!.areAllPermissionsGranted()) {
+                        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                        startActivityForResult(intent, CAMERA)
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread()
+            .check()
+
+
+    }
+
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton(
+                "GO TO SETTINGS"
+            ) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog,
+                                           _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GALLERY) {
+                if (data != null) {
+                    val contentURI = data.data
+                    try {
+                        // Here this is used to get an bitmap from URI
+                        @Suppress("DEPRECATION")
+                        val selectedImageBitmap =
+                            MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
+
+                        binding.ivPlaceImage!!.setImageBitmap(selectedImageBitmap) // Set the selected image from GALLERY to imageView.
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                        Toast.makeText(this@AddMemorialPlaceActivity, "Failed!", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+                // TODO (Step 7: Camera result will be received here.)
+            } else if (requestCode == CAMERA) {
+
+                val thumbnail: Bitmap = data!!.extras!!.get("data") as Bitmap // Bitmap from camera
+                binding.ivPlaceImage!!.setImageBitmap(thumbnail) // Set to the imageView.
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            Log.e("Cancelled", "Cancelled")
+        }
+    }
+
+
+    private fun updateDateInView() {
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        binding.etDate.setText(sdf.format(cal.time).toString())
+    }
+
+
+    companion object {
+        private const val GALLERY = 1
+        private const val CAMERA = 2
+    }
 
 }
