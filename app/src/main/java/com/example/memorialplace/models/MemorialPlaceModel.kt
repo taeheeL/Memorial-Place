9b823e7451c956a328eceabe2273353525d9c0f4
taeheeL <t9h9@naver.com>
2023-02-01 19:13:51 +0900

    build Memorial Place Model

---
+++ app/src/main/java/com/example/memorialplace/models/MemorialPlaceModel.kt
@@ -0,0 +1,12 @@
+package com.example.memorialplace.models
+
+data class MemorialPlaceModel(
+    val id: Int,
+    val title: String,
+    val image: String,
+    val description: String,
+    val date: String,
+    val location: String,
+    val latitude: Double,
+    val longitude: Double
+)
